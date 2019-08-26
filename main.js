6e215a31a2987ca9218a782119f59bb7c02bdcee
RedSparr0w <admin@redsparr0w.com>
2019-08-26 20:22:06 +1200

    Initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+// Modules to control application life and create native browser window
+const electron = require('electron'),
+      { app, BrowserWindow } = electron,
+      fs = require('fs');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  const screenSize = electron.screen.getPrimaryDisplay().workAreaSize,
+    width = 390,
+    height = 1080,
+    x = screenSize.width - width,
+    y = 0;
+
+  mainWindow = new BrowserWindow({
+    width,
+    height,
+    x,
+    y,
+    show: false,
+    backgroundColor: '#222f3e',
+    frame: false,
+    titleBarStyle: 'customButtonsOnHover',
+  });
+
+  // Keep window always on top even with full screen apps
+  //mainWindow.setAlwaysOnTop(true, 'floating');
+  mainWindow.setVisibleOnAllWorkspaces(true);
+  mainWindow.setFullScreenable(false);
+
+  // Load YouTube TV
+  mainWindow.loadURL('https://music.youtube.com/');
+
+  // Inject script and css once loaded
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.webContents.insertCSS(
+      fs.readFileSync(`${__dirname}/renderer.css`).toString()
+    );
+    mainWindow.webContents.executeJavaScript(
+      fs.readFileSync(`${__dirname}/renderer.js`).toString()
+    );
+  });
+
+  // Hide until window has mostly loaded content
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  // Destroy object, close app
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ renderer.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const http = require('http');
+var musicplayer;
+// If you need to pause the music or want to play it, the program wont overite your choice until you next die/spawn.
+var played = false;
+
+let init = setInterval(()=>{
+  if (document.getElementsByTagName('video').length >= 1){
+    clearInterval(init);
+    musicplayer = document.getElementsByTagName('video')[0];
+  }
+},100);
+
+setInterval(()=>{
+  const adSkipButton = (document.getElementsByClassName('ytp-ad-skip-button') || [false])[0];
+  if (adSkipButton) adSkipButton.click();
+}, 100);
+
+http.createServer((req, res) => {
+  if (req.method !== 'POST') return res.end();
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  let body = '';
+  req.on('data', (data) => {
+    body += data;
+  });
+
+  req.on('end', () => {
+    statusUpdate(JSON.parse(body));
+
+    res.end();
+  });
+}).listen(1337);
+
+function statusUpdate (data) {
+  // Discard unwanted messenges
+  if (!musicplayer || !data.previously || !data.player) return;
+
+  /*
+    Music should play when
+    - in menu
+    - dead
+  */
+
+  if (
+    (data.player && data.player.activity === 'menu') || // menu
+    (data.player.state.health === 0 || data.player.steamid !== data.provider.steamid) // dead
+  ) {
+    if (!played && musicplayer.paused){
+      played = true;
+      setTimeout(()=>{
+        musicplayer.play();
+      }, 2500);
+    }
+  } else {
+    if (played && !musicplayer.paused){
+      played = false;
+      setTimeout(()=>{
+        musicplayer.pause();
+      }, 500);
+    }
+  }
+}
+
+window.addEventListener('keypress', ({ key }) => {
+  if (key == '+' || key == '=')
+    musicplayer.volume = musicplayer.volume >= 0.95 ? 1 : +musicplayer.volume.toFixed(2) + 0.05;
+  else if (key == '-')
+    musicplayer.volume = musicplayer.volume <= 0.05 ? 0 : +musicplayer.volume.toFixed(2) - 0.05;
+});
